650c53a9b09f57261f4251caccbf9474f3e5bcfa
Laura Endreß <laura.endress@syntaxphoenix.com>
2021-09-17 15:34:17 +0200

    Even more JavaDoc

--- core/src/main/java/net/sourcewriters/smoothtimber/core/platform/IPlatformFallingBlock.java
+++ core/src/main/java/net/sourcewriters/smoothtimber/core/platform/IPlatformFallingBlock.java
@@ -5,12 +5,12 @@ import net.sourcewriters.smoothtimber.core.util.math.Vector3i;
 public interface IPlatformFallingBlock extends IPlatformEntity {
 
     /**
-     * Drops the item at corresponding to the block type of this entity the specific
-     * position with the specific amount
+     * Drops the item corresponding to the block type of this entity at the
+     * specified position with the specified amount
      * 
-     * @param world
-     * @param vector
-     * @param amount
+     * @param world  the world in which the items should be dropped in
+     * @param vector the position at which the items will be dropped at
+     * @param amount the amount of items that should be dropped
      */
     void dropItemAt(IPlatformWorld world, Vector3i vector, int amount);
 
--- core/src/main/java/net/sourcewriters/smoothtimber/core/platform/IPlatformWorld.java
+++ core/src/main/java/net/sourcewriters/smoothtimber/core/platform/IPlatformWorld.java
@@ -6,42 +6,143 @@ import net.sourcewriters.smoothtimber.core.util.math.Vector3i;
 
 public interface IPlatformWorld {
 
+    /**
+     * Gets the block at a specific position in this world
+     * 
+     * @param  x the x coordinate
+     * @param  y the y coordinate
+     * @param  z the z coordinate
+     * 
+     * @return   the block at the specified position
+     */
     IPlatformBlock getBlock(int x, int y, int z);
 
+    /**
+     * Gets the block at a specific position in this world
+     * 
+     * @param  vector the position of the block
+     * 
+     * @return        the block at the specified position
+     */
     default IPlatformBlock getBlock(Vector3i vector) {
         return getBlock(vector.getX(), vector.getY(), vector.getZ());
     }
 
+    /**
+     * Gets the block at a specific position in this world
+     * 
+     * @param  vector the position of the block
+     * 
+     * @return        the block at the specified position
+     */
     default IPlatformBlock getBlock(Vector3d vector) {
         return getBlock((int) vector.getX(), (int) vector.getY(), (int) vector.getZ());
     }
 
-    void setBlock(int x, int y, int z, IPlatformBlock block);
+    /**
+     * Sets the block at a specific position in this world
+     * 
+     * @param x        the x coordinate
+     * @param y        the y coordinate
+     * @param z        the z coordinate
+     * @param resource the namespaced key of the block that should be placed
+     */
+    void setBlock(int x, int y, int z, ResourceKey resource);
 
-    default void setBlock(Vector3i vector, IPlatformBlock block) {
-        setBlock(vector.getX(), vector.getY(), vector.getZ(), block);
+    /**
+     * Sets the block at a specific position in this world
+     * 
+     * @param vector   the position that the block should be placed at
+     * @param resource the namespaced key of the block that should be placed
+     */
+    default void setBlock(Vector3i vector, ResourceKey resource) {
+        setBlock(vector.getX(), vector.getY(), vector.getZ(), resource);
     }
 
-    default void setBlock(Vector3d vector, IPlatformBlock block) {
-        setBlock((int) vector.getX(), (int) vector.getY(), (int) vector.getZ(), block);
+    /**
+     * Sets the block at a specific position in this world
+     * 
+     * @param vector   the position that the block should be placed at
+     * @param resource the namespaced key of the block that should be placed
+     */
+    default void setBlock(Vector3d vector, ResourceKey resource) {
+        setBlock((int) vector.getX(), (int) vector.getY(), (int) vector.getZ(), resource);
     }
 
+    /**
+     * Spawns the specified entity at the specified position in this world
+     * 
+     * @param  x        the x coordinate
+     * @param  y        the y coordinate
+     * @param  z        the z coordinate
+     * @param  resource the namespaced key of the entity that should be spawned
+     * 
+     * @return          the spawned entity or null if the entity doesn't exist
+     */
     IPlatformEntity spawn(int x, int y, int z, ResourceKey resource);
 
+    /**
+     * Spawns the specified entity at the specified position in this world
+     * 
+     * @param  vector   the position that the entity should be spawned at
+     * @param  resource the namespaced key of the entity that should be spawned
+     * 
+     * @return          the spawned entity or null if the entity doesn't exist
+     */
     default IPlatformEntity spawn(Vector3i vector, ResourceKey resource) {
         return spawn(vector.getX(), vector.getY(), vector.getZ(), resource);
     }
 
+    /**
+     * Spawns the specified entity at the specified position in this world
+     * 
+     * @param  vector   the position that the entity should be spawned at
+     * @param  resource the namespaced key of the entity that should be spawned
+     * 
+     * @return          the spawned entity or null if the entity doesn't exist
+     */
     default IPlatformEntity spawn(Vector3d vector, ResourceKey resource) {
         return spawn((int) vector.getX(), (int) vector.getY(), (int) vector.getZ(), resource);
     }
 
+    /**
+     * Spawns the a falling block entity at the specified position in this world
+     * 
+     * @param  x        the x coordinate
+     * @param  y        the y coordinate
+     * @param  z        the z coordinate
+     * @param  resource the namespaced key of the block type that the falling block
+     *                      should have
+     * 
+     * @return          the spawned falling block entity or null if the block type
+     *                      doesn't exist
+     */
     IPlatformFallingBlock spawnFallingBlock(int x, int y, int z, ResourceKey resource);
 
+    /**
+     * Spawns the a falling block entity at the specified position in this world
+     * 
+     * @param  vector   the position that the falling block should be spawned at
+     * @param  resource the namespaced key of the block type that the falling block
+     *                      should have
+     * 
+     * @return          the spawned falling block entity or null if the block type
+     *                      doesn't exist
+     */
     default IPlatformFallingBlock spawnFallingBlock(Vector3i vector, ResourceKey resource) {
         return spawnFallingBlock(vector.getX(), vector.getY(), vector.getZ(), resource);
     }
 
+    /**
+     * Spawns the a falling block entity at the specified position in this world
+     * 
+     * @param  vector   the position that the falling block should be spawned at
+     * @param  resource the namespaced key of the block type that the falling block
+     *                      should have
+     * 
+     * @return          the spawned falling block entity or null if the block type
+     *                      doesn't exist
+     */
     default IPlatformFallingBlock spawnFallingBlock(Vector3d vector, ResourceKey resource) {
         return spawnFallingBlock((int) vector.getX(), (int) vector.getY(), (int) vector.getZ(), resource);
     }
