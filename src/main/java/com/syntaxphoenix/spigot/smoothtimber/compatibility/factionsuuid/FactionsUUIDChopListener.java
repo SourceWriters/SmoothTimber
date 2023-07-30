f1ac7579ecefc4be93b1ec186ee6f93a0f27e77f
Lauriichan <lauriichan@playuniverse.org>
2023-07-30 17:50:00 +0200

    Improve FactionsUUID support

--- src/main/java/com/syntaxphoenix/spigot/smoothtimber/compatibility/factionsuuid/FactionsUUIDChopListener.java
+++ src/main/java/com/syntaxphoenix/spigot/smoothtimber/compatibility/factionsuuid/FactionsUUIDChopListener.java
@@ -1,27 +1,40 @@
 package com.syntaxphoenix.spigot.smoothtimber.compatibility.factionsuuid;
 
-import com.massivecraft.factions.Board;
-import com.massivecraft.factions.FLocation;
-import com.massivecraft.factions.FPlayer;
 import com.massivecraft.factions.FPlayers;
+import com.massivecraft.factions.FactionsPlugin;
+import com.massivecraft.factions.config.file.MainConfig;
+import com.massivecraft.factions.listeners.FactionsBlockListener;
 import com.massivecraft.factions.perms.PermissibleActions;
 import com.syntaxphoenix.spigot.smoothtimber.event.AsyncPlayerChopTreeEvent;
 import com.syntaxphoenix.spigot.smoothtimber.event.reason.DefaultReason;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 public class FactionsUUIDChopListener implements Listener {
+    
+    private MainConfig config;
+    private FPlayers players;
+    
+    public FactionsUUIDChopListener() {
+        this.config = FactionsPlugin.getInstance().conf();
+        this.players = FPlayers.getInstance();
+    }
 
     @EventHandler
     public void onAsyncPlayerChopTree(final AsyncPlayerChopTreeEvent event) {
-        FPlayer player = FPlayers.getInstance().getByPlayer(event.getPlayer());
-        if (event.getBlockLocations().stream()
-                .map(FLocation::new)
-                .distinct()
-                .anyMatch(location -> !Board.getInstance().getFactionAt(location).hasAccess(player, PermissibleActions.DESTROY, location))
-        ) {
-            event.setCancelled(true);
-            event.setReason(DefaultReason.FACTIONS);
+        Player player = event.getPlayer();
+        if (config.factions().protection().getPlayersWhoBypassAllProtection().contains(player.getName()) || players.getByPlayer(player).isAdminBypassing()) {
+            return;
+        }
+        for(Location bktLocation : event.getBlockLocations()) {
+            if (!FactionsBlockListener.playerCanBuildDestroyBlock(event.getPlayer(), bktLocation, PermissibleActions.DESTROY, true)) {
+                event.setCancelled(true);
+                event.setReason(DefaultReason.FACTIONS);
+                return;
+            }
         }
     }
 
